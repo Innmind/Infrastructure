f80153f8c6fc152b15214e06e4c31765efd64df5
Baptiste Langlade <baptouuuu@gmail.com>
2020-07-01 19:33:08 +0200

    add gene to install transmission

---
+++ src/MacOS/Gene/Transmission.php
@@ -0,0 +1,109 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Infrastructure\MacOS\Gene;
+
+use Innmind\Genome\{
+    Gene,
+    History,
+    Exception\PreConditionFailed,
+    Exception\ExpressionFailed,
+};
+use Innmind\OperatingSystem\OperatingSystem;
+use Innmind\Server\Control\{
+    Server,
+    Server\Command,
+    Server\Script,
+    Exception\ScriptFailed,
+};
+use Innmind\Url\Path;
+use Innmind\TimeContinuum\Earth\Period\Second;
+
+final class Transmission implements Gene
+{
+    private string $version;
+
+    private function __construct(string $version)
+    {
+        $this->version = $version;
+    }
+
+    public static function install(string $version): self
+    {
+        return new self($version);
+    }
+
+    public function name(): string
+    {
+        return "Transmission {$this->version} install";
+    }
+
+    public function express(
+        OperatingSystem $local,
+        Server $target,
+        History $history
+    ): History {
+        try {
+            $preCondition = new Script(
+                Command::foreground('which')->withArgument('curl'),
+            );
+            $preCondition($target);
+        } catch (ScriptFailed $e) {
+            throw new PreConditionFailed('curl is missing');
+        }
+
+        try {
+            $download = new Script(
+                Command::foreground('curl')
+                    ->withShortOption('O')
+                    ->withShortOption('L')
+                    ->withShortOption('C')
+                    ->withArgument('-')
+                    ->withArgument("https://github.com/transmission/transmission-releases/raw/master/Transmission-{$this->version}.dmg")
+                    ->withWorkingDirectory(Path::of('Downloads')),
+                Command::foreground('open')
+                    ->withArgument("Transmission-{$this->version}.dmg")
+                    ->withWorkingDirectory(Path::of('Downloads')),
+            );
+            $download($target);
+
+            $this->wait($local, $target);
+
+            $install = new Script(
+                Command::foreground('cp')
+                    ->withShortOption('r') // because the app is a directory
+                    ->withArgument('/Volumes/Transmission/Transmission.app')
+                    ->withArgument('/Applications/'),
+                Command::foreground('diskutil')
+                    ->withArgument('unmountDisk')
+                    ->withArgument('/Volumes/Transmission'),
+                Command::foreground('rm')
+                    ->withArgument("Transmission-{$this->version}.dmg")
+                    ->withWorkingDirectory(Path::of('Downloads')),
+            );
+            $install($target);
+        } catch (ScriptFailed $e) {
+            throw new ExpressionFailed($this->name());
+        }
+
+        return $history;
+    }
+
+    private function wait(OperatingSystem $local, Server $target): void
+    {
+        do {
+            try {
+                $checkItExist = new Script(
+                    Command::foreground('test')
+                        ->withShortOption('d')
+                        ->withArgument('/Volumes/Transmission/'),
+                );
+                $checkItExist($target);
+
+                return;
+            } catch (ScriptFailed $e) {
+                $local->process()->halt(new Second(1));
+            }
+        } while (true);
+    }
+}
