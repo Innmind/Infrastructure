f20026679020a9368d817714bface840b63c09b7
Baptiste Langlade <baptouuuu@gmail.com>
2020-07-02 19:23:35 +0200

    add micro snitch gene

---
+++ src/MacOS/Gene/MicroSnitch.php
@@ -0,0 +1,122 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Infrastructure\MacOS\Gene;
+
+use Innmind\Genome\{
+    Gene,
+    History,
+    Exception\PreConditionFailed,
+    Exception\ExpressionFailed,
+};
+use Innmind\OperatingSystem\OperatingSystem;
+use Innmind\Server\Control\{
+    Server,
+    Server\Command,
+    Server\Script,
+    Exception\ScriptFailed,
+};
+use Innmind\Url\Path;
+use Innmind\TimeContinuum\Earth\Period\Second;
+
+final class MicroSnitch implements Gene
+{
+    private string $version;
+
+    private function __construct(string $version)
+    {
+        $this->version = $version;
+    }
+
+    public static function install(string $version): self
+    {
+        return new self($version);
+    }
+
+    public function name(): string
+    {
+        return "Micro Snitch install";
+    }
+
+    public function express(
+        OperatingSystem $local,
+        Server $target,
+        History $history
+    ): History {
+        try {
+            $alreadyExist = new Script(
+                Command::foreground('test')
+                    ->withShortOption('d')
+                    ->withArgument('Micro Snitch.app')
+                    ->withWorkingDirectory(Path::of('/Applications/')),
+            );
+            $alreadyExist($target);
+
+            return $history;
+        } catch (ScriptFailed $e) {
+            // do not exist, trying to install
+        }
+
+        try {
+            $preCondition = new Script(
+                Command::foreground('which')->withArgument('curl'),
+            );
+            $preCondition($target);
+        } catch (ScriptFailed $e) {
+            throw new PreConditionFailed('curl is missing');
+        }
+
+        try {
+            $download = new Script(
+                Command::foreground('curl')
+                    ->withShortOption('o')
+                    ->withArgument('microsnitch.zip')
+                    ->withShortOption('L')
+                    ->withShortOption('C')
+                    ->withArgument('-')
+                    ->withArgument("https://www.obdev.at/ftp/pub/Products/MicroSnitch/MicroSnitch-{$this->version}.zip")
+                    ->withWorkingDirectory(Path::of('Downloads')),
+                Command::foreground('open')
+                    ->withArgument('microsnitch.zip')
+                    ->withWorkingDirectory(Path::of('Downloads')),
+            );
+            $download($target);
+
+            $this->wait($local, $target);
+
+            $install = new Script(
+                Command::foreground('mv')
+                    ->withArgument('Micro Snitch.app')
+                    ->withArgument('/Applications/')
+                    ->withWorkingDirectory(Path::of('Downloads/')),
+                Command::foreground('rm')
+                    ->withArgument('microsnitch.zip')
+                    ->withWorkingDirectory(Path::of('Downloads')),
+            );
+            $install($target);
+        } catch (ScriptFailed $e) {
+            throw new ExpressionFailed($this->name());
+        }
+
+        return $history;
+    }
+
+    private function wait(OperatingSystem $local, Server $target): void
+    {
+        do {
+            try {
+                $checkItExist = new Script(
+                    Command::foreground('test')
+                        ->withShortOption('d')
+                        ->withArgument('Micro Snitch.app')
+                        ->withWorkingDirectory(Path::of('Downloads/')),
+                );
+                $checkItExist($target);
+
+                return;
+            } catch (ScriptFailed $e) {
+                $local->process()->halt(new Second(1));
+            }
+        } while (true);
+    }
+}
