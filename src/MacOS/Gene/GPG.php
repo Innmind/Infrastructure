fdd649374c29d283d212285c2acce00911ea519d
Baptiste Langlade <baptouuuu@gmail.com>
2020-07-02 18:44:30 +0200

    add gpg suite gene

---
+++ src/MacOS/Gene/GPG.php
@@ -0,0 +1,116 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Infrastructure\MacOS\Gene;
+
+use Innmind\Genome\{
+    Gene,
+    History,
+    Exception\PreConditionFailed,
+    Exception\ExpressionFailed,
+};
+use Innmind\OperatingSystem\OperatingSystem;
+use Innmind\Server\Control\{
+    Server,
+    Server\Command,
+    Server\Script,
+    Exception\ScriptFailed,
+};
+use Innmind\Url\Path;
+use Innmind\TimeContinuum\Earth\Period\Second;
+
+final class GPG implements Gene
+{
+    private string $version;
+
+    private function __construct(string $version)
+    {
+        $this->version = $version;
+    }
+
+    public static function install(string $version): self
+    {
+        return new self($version);
+    }
+
+    public function name(): string
+    {
+        return 'GPG install';
+    }
+
+    public function express(
+        OperatingSystem $local,
+        Server $target,
+        History $history
+    ): History {
+        try {
+            $alreadyExist = new Script(
+                Command::foreground('test')
+                    ->withShortOption('d')
+                    ->withArgument('GPG Keychain.app')
+                    ->withWorkingDirectory(Path::of('/Applications/')),
+            );
+            $alreadyExist($target);
+
+            // return $history;
+        } catch (ScriptFailed $e) {
+            // do not exist, trying to install
+        }
+
+        try {
+            $preCondition = new Script(
+                Command::foreground('which')->withArgument('curl'),
+            );
+            $preCondition($target);
+        } catch (ScriptFailed $e) {
+            throw new PreConditionFailed('curl is missing');
+        }
+
+        try {
+            $download = new Script(
+                Command::foreground('curl')
+                    ->withShortOption('o')
+                    ->withArgument('GPG_Suite.dmg')
+                    ->withShortOption('L')
+                    ->withShortOption('C')
+                    ->withArgument('-')
+                    ->withArgument("https://releases.gpgtools.org/GPG_Suite-{$this->version}.dmg")
+                    ->withWorkingDirectory(Path::of('Downloads')),
+                Command::foreground('open')
+                    ->withArgument('GPG_Suite.dmg')
+                    ->withWorkingDirectory(Path::of('Downloads')),
+            );
+            $download($target);
+
+            $this->wait($local, $target);
+
+            $install = new Script(
+                Command::foreground('open')
+                    ->withArgument('/Volumes/GPG Suite/Install.pkg'),
+            );
+            $install($target);
+        } catch (ScriptFailed $e) {
+            throw new ExpressionFailed($this->name());
+        }
+
+        return $history;
+    }
+
+    private function wait(OperatingSystem $local, Server $target): void
+    {
+        do {
+            try {
+                $checkItExist = new Script(
+                    Command::foreground('test')
+                        ->withShortOption('d')
+                        ->withArgument('/Volumes/GPG Suite/'),
+                );
+                $checkItExist($target);
+
+                return;
+            } catch (ScriptFailed $e) {
+                $local->process()->halt(new Second(1));
+            }
+        } while (true);
+    }
+}
