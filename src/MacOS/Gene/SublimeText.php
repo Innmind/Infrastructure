17293a0a0254265b453e2815065958aab223a074
Baptiste Langlade <baptouuuu@gmail.com>
2020-07-02 19:08:56 +0200

    add sublime text gene

---
+++ src/MacOS/Gene/SublimeText.php
@@ -0,0 +1,124 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Infrastructure\MacOS\Gene;
+
+use Innmind\Genome\{
+    Gene,
+    History,
+    Exception\PreConditionFailed,
+    Exception\ExpressionFailed,
+};
+use Innmind\OperatingSystem\OperatingSystem;
+use Innmind\Server\Control\{
+    Server,
+    Server\Command,
+    Server\Script,
+    Exception\ScriptFailed,
+};
+use Innmind\Url\Path;
+use Innmind\TimeContinuum\Earth\Period\Second;
+
+final class SublimeText implements Gene
+{
+    private string $version;
+
+    private function __construct(string $version)
+    {
+        $this->version = $version;
+    }
+
+    public static function install(string $version): self
+    {
+        return new self($version);
+    }
+
+    public function name(): string
+    {
+        return "Sublime Text install";
+    }
+
+    public function express(
+        OperatingSystem $local,
+        Server $target,
+        History $history
+    ): History {
+        try {
+            $alreadyExist = new Script(
+                Command::foreground('test')
+                    ->withShortOption('d')
+                    ->withArgument('Sublime Text.app')
+                    ->withWorkingDirectory(Path::of('/Applications/')),
+            );
+            $alreadyExist($target);
+
+            return $history;
+        } catch (ScriptFailed $e) {
+            // do not exist, trying to install
+        }
+
+        try {
+            $preCondition = new Script(
+                Command::foreground('which')->withArgument('curl'),
+            );
+            $preCondition($target);
+        } catch (ScriptFailed $e) {
+            throw new PreConditionFailed('curl is missing');
+        }
+
+        try {
+            $download = new Script(
+                Command::foreground('curl')
+                    ->withShortOption('o')
+                    ->withArgument('sublimetext.dmg')
+                    ->withShortOption('L')
+                    ->withShortOption('C')
+                    ->withArgument('-')
+                    ->withArgument("https://download.sublimetext.com/Sublime Text Build {$this->version}.dmg")
+                    ->withWorkingDirectory(Path::of('Downloads')),
+                Command::foreground('open')
+                    ->withArgument('sublimetext.dmg')
+                    ->withWorkingDirectory(Path::of('Downloads')),
+            );
+            $download($target);
+
+            $this->wait($local, $target);
+
+            $install = new Script(
+                Command::foreground('cp')
+                    ->withShortOption('r') // because the app is a directory
+                    ->withArgument('/Volumes/Sublime Text/Sublime Text.app')
+                    ->withArgument('/Applications/'),
+                Command::foreground('diskutil')
+                    ->withArgument('unmountDisk')
+                    ->withArgument('/Volumes/Sublime Text'),
+                Command::foreground('rm')
+                    ->withArgument('sublimetext.dmg')
+                    ->withWorkingDirectory(Path::of('Downloads')),
+            );
+            $install($target);
+        } catch (ScriptFailed $e) {
+            throw new ExpressionFailed($this->name());
+        }
+
+        return $history;
+    }
+
+    private function wait(OperatingSystem $local, Server $target): void
+    {
+        do {
+            try {
+                $checkItExist = new Script(
+                    Command::foreground('test')
+                        ->withShortOption('d')
+                        ->withArgument('/Volumes/Sublime Text/'),
+                );
+                $checkItExist($target);
+
+                return;
+            } catch (ScriptFailed $e) {
+                $local->process()->halt(new Second(1));
+            }
+        } while (true);
+    }
+}
